b6a9a21a0586143aff855117fb02d338c32b6e2f
HB <hbianan@gmail.com>
2019-08-23 21:31:33 -0400

    retrieved file

--- config/config.js
+++ config/config.js
@@ -1,12 +1,16 @@
 module.exports = {
   "development": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  },
-  "test": {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": "db_emotions",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
